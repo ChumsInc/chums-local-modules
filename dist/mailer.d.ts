5ae24a2c8cc556d66f24596a10e90c18bc127968
Steve Montgomery <steve@chums.com>
2023-05-03 10:44:49 -0600

    fix: remove deprecated email via AWS:SPS service.

--- dist/mailer.d.ts
+++ dist/mailer.d.ts
@@ -13,25 +13,6 @@ export interface sendMailProps {
     textContent?: string;
     attachments?: any;
 }
-/**
- * The following environment variables are required:
- *     <div>
- *         <strong>AMAZON_SES_HOST</strong>
- *         <strong>AMAZON_SES_PORT</strong>
- *         <strong>AMAZON_SES_USERNAME</strong>
- *         <strong>AMAZON_SES_PASSWORD</strong>
- *     </div>
- * @param {string|string[]} [to]
- * @param {string|string[]} [cc]
- * @param {string|string[]} [bcc]
- * @param {string} replyTo
- * @param {string} from
- * @param {string} subject
- * @param {string} html
- * @param {string} [textContent]
- * @param [attachments]
- */
-export declare const sendOldSESEmail: ({ to, cc, bcc, replyTo, from, subject, html, textContent, attachments }: sendMailProps) => Promise<import("nodemailer/lib/smtp-transport").SentMessageInfo>;
 export declare const getTs: () => number;
 export declare const getTs36: () => string;
 /**
--- dist/mailer.js
+++ dist/mailer.js
@@ -1,73 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendEmail = exports.sendGmail = exports.getLogoImageAttachment = exports.getTs36 = exports.getTs = exports.sendOldSESEmail = void 0;
+exports.sendEmail = exports.sendGmail = exports.getLogoImageAttachment = exports.getTs36 = exports.getTs = void 0;
 const debug_1 = require("debug");
 const nodemailer_1 = require("nodemailer");
 const debug = (0, debug_1.default)('chums:lib:mailer');
-/**
- * The following environment variables are required:
- *     <div>
- *         <strong>AMAZON_SES_HOST</strong>
- *         <strong>AMAZON_SES_PORT</strong>
- *         <strong>AMAZON_SES_USERNAME</strong>
- *         <strong>AMAZON_SES_PASSWORD</strong>
- *     </div>
- * @param {string|string[]} [to]
- * @param {string|string[]} [cc]
- * @param {string|string[]} [bcc]
- * @param {string} replyTo
- * @param {string} from
- * @param {string} subject
- * @param {string} html
- * @param {string} [textContent]
- * @param [attachments]
- */
-const sendOldSESEmail = async ({ to = [], cc = [], bcc = [], replyTo, from, subject, html, textContent, attachments }) => {
-    try {
-        to = !Array.isArray(to) ? [to] : to;
-        cc = !Array.isArray(cc) ? [cc] : cc;
-        bcc = !Array.isArray(bcc) ? [bcc] : bcc;
-        if (!from) {
-            from = `"Chums AutoMailer" <automated@chums.com>`;
-        }
-        if (replyTo && !(cc.includes(replyTo) || bcc.includes(replyTo))) {
-            cc.push(replyTo);
-        }
-        process.env.AWS_ACCESS_KEY_ID = process.env.AMAZON_SES_USERNAME;
-        process.env.AWS_SECRET_ACCESS_KEY = process.env.AMAZON_SES_PASSWORD;
-        const transporter = (0, nodemailer_1.createTransport)({
-            host: process.env.AMAZON_SES_HOST,
-            port: Number(process.env.AMAZON_SES_PORT),
-            secure: true,
-            auth: {
-                user: process.env.AMAZON_SES_USERNAME,
-                pass: process.env.AMAZON_SES_PASSWORD
-            }
-        });
-        let mailOptions = {
-            from,
-            to,
-            cc,
-            bcc,
-            replyTo,
-            subject,
-            html,
-            text: textContent,
-            attachments
-        };
-        // return mailOptions;
-        return await transporter.sendMail(mailOptions);
-    }
-    catch (err) {
-        if (err instanceof Error) {
-            debug("sendEmail()", err.message);
-            return Promise.reject(err);
-        }
-        debug("sendEmail()", err);
-        return Promise.reject(err);
-    }
-};
-exports.sendOldSESEmail = sendOldSESEmail;
 const getTs = () => {
     return Date.now();
 };
