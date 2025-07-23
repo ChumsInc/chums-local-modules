e91427cc4cb9c928ab909aa5f0fd7cf546556305
Steve Montgomery <steve@chums.com>
2025-07-23 08:25:09 -0600

    Add logPath utility, updated error messages for failed validation.

--- dist/log-path.js
+++ dist/log-path.js
@@ -1,6 +1,6 @@
 export const logPath = (debug) => (req, res, next) => {
     const { ip, method, originalUrl } = req;
-    const user = res.locals.profile?.user ?? res.locals?.profile?.user?.id ?? '-';
+    const user = res.locals.profile?.user?.email ?? res.locals?.profile?.user?.id ?? '-';
     const referer = req.get('referer') || '';
     debug(ip, user, method, originalUrl, referer);
     next();
--- lib/log-path.ts
+++ lib/log-path.ts
@@ -4,7 +4,7 @@ import Debug from "debug";
 
 export const logPath = (debug: Debug.Debugger) => (req: Request, res: Response, next: NextFunction) => {
     const {ip, method, originalUrl} = req;
-    const user = res.locals.profile?.user ?? res.locals?.profile?.user?.id ?? '-';
+    const user = res.locals.profile?.user?.email ?? res.locals?.profile?.user?.id ?? '-';
     const referer = req.get('referer') || '';
     debug(ip, user, method, originalUrl, referer);
     next();
