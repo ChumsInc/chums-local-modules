729464fa1b958aa5107a98cd6e68d6edd30af6f0
Steve Montgomery <steve@chums.com>
2026-09-11 11:46:33 -0600

    feat: add handlers for fixing API usage paths and parameters
    - Introduced `fix-handlers.ts` with functions to load and update API usage fixes.
    - Added SQL queries to identify and correct invalid paths and parameters.
    - Included a test script `test-fix.ts` for verification.

---
+++ utils/fix-api-usage/fix-handlers.ts
@@ -0,0 +1,77 @@
+import 'dotenv/config';
+import Debug from 'debug';
+import type {RowDataPacket} from "mysql2";
+import {mysql2Pool} from "../../dist/index.js";
+
+const debug = Debug('chums:utils:fix-api-usage:fix-handlers');
+
+interface APIFix {
+    id: number;
+    path: string;
+    params: Record<string, string|number>;
+    path_match?: string;
+    reportAs?: string;
+}
+type APIFixRow = APIFix & RowDataPacket;
+const sql = `
+    SELECT h.id, h.api, h.path, JSON_EXTRACT(IFNULL(h.params, '{}'), '$') AS params, p.path_match, p.reportAs
+    FROM users.api_usage_history h
+             LEFT JOIN users.api_path p ON h.path REGEXP p.path_match
+    WHERE h.api = 'api-operations'
+      AND h.id < 95000
+      AND p.id = :id
+      and JSON_VALUE(h.params, '$.customer_id') IS NULL`;
+
+const updateLog = `
+UPDATE users.api_usage_history
+SET path = :fixedPath, params = :fixedParams
+where id = :id`;
+
+export async function loadAPIFixData(id: number):Promise<APIFix[]> {
+    try {
+        const [rows] = await mysql2Pool.query<APIFixRow[]>(sql, {id});
+        return rows.map(row => {
+            const regExp = new RegExp(row.path_match ?? '', 'i');
+            const match = regExp.exec(row.path);
+            console.log(row.path, match);
+            const fix = row.params ?? {};
+            if (!fix.customer_id && match && match.length >= 3) {
+                fix.customer_id = match[1];
+                fix.id = match[2];
+            }
+            return {
+                id: row.id,
+                path: '/barcodes/customers/:customer_id/items/:id.json',
+                params: {...fix},
+            }
+        });
+    } catch(err:unknown) {
+        if (err instanceof Error) {
+            debug("loadAPIFixData()", err.message);
+            return Promise.reject(err);
+        }
+        debug("loadAPIFixData()", err);
+        return Promise.reject(new Error('Error in loadAPIFixData()'));
+    }
+}
+
+export async function updateAPIUsage(id: number) {
+    try {
+        const fixes = await loadAPIFixData(id);
+        await Promise.allSettled(fixes.map(async fix => {
+            await mysql2Pool.query(updateLog, {
+                fixedPath: fix.path,
+                fixedParams: JSON.stringify(fix.params),
+                id: fix.id
+            });
+        }))
+        return {fixed: fixes.length};
+    } catch(err:unknown) {
+        if (err instanceof Error) {
+            debug("updateAPIUsage()", err.message);
+            return Promise.reject(err);
+        }
+        debug("updateAPIUsage()", err);
+        return Promise.reject(new Error('Error in updateAPIUsage()'));
+    }
+}
---
+++ utils/fix-api-usage/test-fix.ts
@@ -0,0 +1,9 @@
+import {loadAPIFixData} from "./fix-handlers.js";
+
+loadAPIFixData(6)
+    .catch(err => console.error(err))
+    .then(data => console.log(data));
+
+// updateAPIUsage(4)
+//     .catch(err => console.error(err))
+//     .then(data => console.log(data));
